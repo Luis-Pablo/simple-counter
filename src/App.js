208f7cca78d6df47d7c213196b1b14f550703c81
Luis Pablo <pabloarias024@gmail.com>
2023-03-07 16:15:03 -0300

    reloj funcionando, falta favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Contador from './components/contador';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faClock } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container text-center mt-5 bg-dark py-5">
+      <div className="row text-white fs-1 ">
+        <div className="col">
+          
+        </div>
+        <div className="col">
+          {Math.floor(props.numero / 3600)}
+        </div>
+        <div className="col">
+          {Math.floor(props.numero / 600)}
+        </div>
+        <div className="col">
+          {Math.floor(props.numero / 60)}
+        </div>
+        <div className="col">
+          {Math.floor(props.numero % 10)}
+        </div>
+
+      </div>
     </div>
+
   );
 }
 
 export default App;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let time = 0;
+setInterval(() => {
+  time++;
+  root.render(
+    <React.StrictMode>
+      <App numero={time } />
+    </React.StrictMode>
+  );
+}, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
